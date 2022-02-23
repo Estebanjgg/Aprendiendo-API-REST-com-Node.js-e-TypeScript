b620d1e3b004b4a1c9680465778d87a59f923e8d
esteban <esteban030991@gmail.com>
2022-02-23 15:11:30 -0300

    Primeros pasos con API REST Node.js TypesCript

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import  Express from  'express'
+import { router } from './routes'
+
+const app = Express()
+
+app.use(Express.json())
+app.use(router)
+
+export { app }
---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,39 @@
+import { User } from "../../entities/Users";
+import { IMailProvider } from "../../providers/IMailProvider";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
+export class CreateUserUseCase {
+
+    constructor(
+        private usersRepository: IUserRepository,
+        private mailProvider: IMailProvider,
+    ) { }
+
+    async execute(data: ICreateUserRequestDTO) {
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+
+        if (userAlreadyExists) {
+            throw new Error('User alredy exists.');
+        }
+        const user = new User(data);
+
+        await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+
+            },
+            from: {
+                name: 'Equipe do meu App',
+                email: 'equipe@meuapp.com',
+            },
+            subject: 'Seja bem-vindo á plataforma',
+            body: '<p>Voce ja pode Fazer login en nossa plataforma.</p>'
+        })
+
+    }
+}
